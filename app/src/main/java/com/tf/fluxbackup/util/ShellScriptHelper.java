d26c44cb3ab988c66bcd53bc398f35b516092778
Kamran <team.radiant@gmail.com>
2016-04-03 19:33:07 +0530

    Return shell execution result

--- app/src/main/java/com/tf/fluxbackup/util/ShellScriptHelper.java
+++ app/src/main/java/com/tf/fluxbackup/util/ShellScriptHelper.java
@@ -1,20 +1,40 @@
 package com.tf.fluxbackup.util;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by kamran on 4/3/16.
  */
 public class ShellScriptHelper {
     
-    static void executeShell(String command) throws IOException, InterruptedException {
+    static String executeShell(String command) throws IOException, InterruptedException {
         Process process = Runtime.getRuntime().exec("su");
+
         DataOutputStream os = new DataOutputStream(process.getOutputStream());
         os.writeBytes(command);
-//        os.writeBytes("\nexit\n");
+        os.writeBytes("\nexit\n");
+
         os.flush();
         os.close();
         process.waitFor();
+
+        return inputStreamToString(process.getInputStream());
+    }
+
+    public static String inputStreamToString(InputStream stream) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+        String read;
+
+        while ((read = bufferedReader.readLine()) != null) {
+            stringBuilder.append(read);
+        }
+
+        bufferedReader.close();
+        return stringBuilder.toString();
     }
 }
