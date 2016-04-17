a230cfafcdac0954e69ae1a8bb353ae2a3f51643
Kamran <team.radiant@gmail.com>
2016-04-17 16:12:10 +0530

    Use the real sdcard path

--- app/src/main/java/com/tf/fluxbackup/util/Constants.java
+++ app/src/main/java/com/tf/fluxbackup/util/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final int NOTIFICATION_BACKUP = 49;
     public static final int NOTIFICATION_RESTORE = 50;
 
-    public static final String APP_DATA_LOCATION = "/sdcard/team.fluxion/";
+    public static final String APP_DATA_LOCATION = System.getenv("EXTERNAL_STORAGE") + "/team.fluxion/";
     public static final String BACKUP_LOCATION = APP_DATA_LOCATION + "FluxBackup/backups/";
     public static final String CACHE_LOCATION = APP_DATA_LOCATION + "FluxBackup/cache/";
 
